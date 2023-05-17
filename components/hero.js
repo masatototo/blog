1aacf2c670fe5f9ba7278c27851bdb22a970995f
masatototo <m.sasaki3891@gmail.com>
2023-05-17 15:11:53 +0900

    rsponsive

--- components/hero.js
+++ components/hero.js
@@ -1,13 +1,22 @@
 import styles from 'styles/hero.module.css'
+import Image  from 'next/image'
+import cube from 'images/cube.jpg'
 
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <div>
+        <div className={styles.flexContainer}>
             <div className={styles.text}>
             <h1 className={styles.title}>{title}</h1>
             <p className={styles.subtitle}>{subtitle}</p>
             </div>
-            {imageOn && <figure> [画像] </figure>}
+            {imageOn && (
+            <figure>
+                <Image src={cube} alt="" sizes='100vw' style={{ width: '100%', height: 'auto',}}
+                priority
+                placeholder='blur'
+                /> 
+            </figure>
+            )}
         </div>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,61 @@
+import Container from "components/container";
 import Hero from "components/hero";
+import PostBody from "components/post-body";
+import Contact from "components/contact";
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import Image from "next/image";
+import eyecatch from 'images/about.jpg'
 
 export default function About() {
     return  (
-    <Hero
-     title="About"
-     subtitle="About devlopment activities"
-     />
+        <Container>
+            <Hero title="About" subtitle="About devlopment activities"/>
+
+            <figure>
+                <Image
+                  src={eyecatch}
+                  alt=""
+                //   layout="responsive"
+                  sizes="100vw"
+                  style={{
+                    width: '100%',
+                    height: 'auto',
+                  }}
+                  priority
+                  placeholder="blur"
+                  />
+            </figure>
+       <TwoColumn>
+        <TwoColumnMain>
+        <PostBody>
+            <p>
+                Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さ
+                まざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
+            </p>
+            <h2>モノづくりで目指していること</h2>
+            <p>
+                モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入
+                れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、
+                特にそこを磨く作業に力を入れています。
+            </p>
+            <p>
+                単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしてい
+                ます。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分
+                の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
+            </p>
+            <h3>新しいことへのチャレンジ</h3>
+            <p>
+                今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから
+                取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづ
+                くりは、これからも続けていきたいです。
+            </p>
+          </PostBody>
+         </TwoColumnMain>
+
+        <TwoColumnSidebar>
+         <Contact />
+        </TwoColumnSidebar>
+       </TwoColumn>
+    </Container>
     )
 }
